5437c5d03399602fd367afe0ffe29ee0d68eefc4
amelia2908 <166909728+amelia2908@users.noreply.github.com>
2024-05-04 17:54:59 +0700

    hahahehe
    ko co gi het trolls vn

---
+++ lesson_1/main.js
@@ -0,0 +1,48 @@
+// ten bien = gia tri
+var teacherName = 'doan';
+var studentName = 'Amelia';
+var age = 10;
+
+// alert = hien thong bao
+
+// built in
+
+// alert(studentName);
+// alert(age);
+// alert(teacherName);
+
+
+console.log(studentName);
+
+console.warn(studentName);
+
+console.error(studentName);
+
+confirm('Xac nhan du tuoi!>:)');
+
+prompt('Xac nhan du tuoi!>:)');
+
+
+
+// toan tu so
+
+//toan tu so hoc=phep tinh +, -, *, /    :
+
+var a = 1 + 2;
+console.log(a);
+//toan tu gan = dat ten
+var a2 = 1;
+
+//toan tu so sanh=so sanh
+var a2 = 2;
+var b2 = 1;
+if( a2>b2 ){
+    alert('Dung');
+}
+
+//toan tu logic=2 cai phep tinh
+var a3 = 1;
+var b3 = 2;
+if( a3 > 0 && b3 > 0 ){
+    alert('a3 & b3 lon hon 0');
+}
